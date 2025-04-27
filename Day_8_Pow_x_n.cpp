86a9fcebaf53495c1ac0880b9c7f4d4f4ba6c611
Tanmay <68606780+TnmyB@users.noreply.github.com>
2025-04-27 17:23:04 +0530

    Update Day_8_Pow_x_n.cpp

--- Day_8_Pow_x_n.cpp
+++ Day_8_Pow_x_n.cpp
@@ -32,3 +32,31 @@ public:
         return ans;
     }
 };
+
+
+// Approach - O(log N)
+// double your base by halving your power
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1.0;
+        long long nn = n;
+        if(nn < 0 ){
+            nn = nn * -1;
+        }
+        while(nn != 0){
+            if(nn % 2){
+                ans = ans * x;
+                nn--;
+            } else{
+                x = x * x;
+                nn = nn / 2;
+            }
+        }
+
+        if(n < 0){
+            ans = (double) (1.0) / (double) (ans);
+        }
+        return ans;
+    }
+};
