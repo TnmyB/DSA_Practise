704e22bf342012f2ad01e02b3852854272cbff52
Tanmay <68606780+TnmyB@users.noreply.github.com>
2025-04-28 07:49:44 +0530

    Update Day_9_Unique_Paths.cpp

--- Day_9_Unique_Paths.cpp
+++ Day_9_Unique_Paths.cpp
@@ -1,4 +1,50 @@
-// Grid Problem in P&C
+// Brute force through recursion - gives TLE for high input as both SC and TC are exponential
+// https://leetcode.com/problems/unique-paths/description/
+class Solution {
+public:
+    int iteratePaths(int startM, int startN, int m, int n){
+        int path = 0;
+        if(startM > m || startN > n){
+            return 0;
+        }
+        if(startM == m && startN == n){
+            return path + 1;
+        }
+        path += iteratePaths(startM + 1, startN, m, n);
+        path += iteratePaths(startM, startN + 1, m, n);
+        return path;
+
+    }
+    int uniquePaths(int m, int n) {
+        return iteratePaths(0, 0, m-1, n-1);
+    }
+};
+
+// Through DP
+// Convert recursion into DP
+// TC - O(m*n) SC - O(m*n)
+class Solution {
+public:
+    int iteratePaths(int startM, int startN, int m, int n, vector<vector<int>>& dp){
+        if(startM > m || startN > n){
+            return 0;
+        }
+        if(startM == m && startN == n){
+            return 1;
+        }
+        if(dp[startM][startN] != -1){
+            return dp[startM][startN];
+        }
+        return dp[startM][startN] = iteratePaths(startM + 1, startN, m, n , dp) + iteratePaths(startM, startN + 1, m, n, dp);
+    }
+    
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return iteratePaths(0, 0, m-1, n-1, dp);
+    }
+};
+
+// Grid Problem P&C solution
 class Solution {
 public:
     int uniquePaths(int m, int n) {
