6110237a49b7e9d25671ad2180e8d7fe414cbebc
Tanmay <68606780+TnmyB@users.noreply.github.com>
2025-05-04 10:10:30 +0530

    Update Day_15_Delete_Node_In_A_Linked_List.cpp

--- LinkedList/Day_15_Delete_Node_In_A_Linked_List.cpp
+++ LinkedList/Day_15_Delete_Node_In_A_Linked_List.cpp
@@ -1,5 +1,7 @@
 // https://leetcode.com/problems/delete-node-in-a-linked-list/description/
 
+
+// Dumb ways to die - over engineered solution
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -20,5 +22,21 @@ public:
         node -> val = nextNode -> val;
         node -> next = nextNode -> next;
 
+    }
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node -> val = node -> next -> val;
+        node -> next = node -> next -> next;
     }
 };
